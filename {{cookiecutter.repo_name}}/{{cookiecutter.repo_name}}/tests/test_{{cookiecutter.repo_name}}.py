20a0eb98803b7082fbbbdbafd93dfa4137bf344e
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-07-21 10:07:09 -0700

    Opinionated changes (#1)
    * don't use lgtm
    
    * drop 3.8, add 3.11
    
    * update codecov action + only upload results on PRs and merge
    
    * Remove setup.cfg in favor of pyproject.toml
    
    * add pre-commit
    
    * properlly escape {{
    
    * properlly escape {{ and add a space
    
    * Added os files to git ignore
    
    * make init file 88 chars in lenght
    
    * fix some flake8 issues

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -12,4 +12,15 @@ import {{cookiecutter.repo_name}}
 
 def test_{{cookiecutter.repo_name}}_imported():
     """Sample test, will always pass so long as import statement worked."""
+    print("importing ", {{cookiecutter.repo_name}}.__name__)
     assert "{{cookiecutter.repo_name}}" in sys.modules
+
+
+# Assert that a certain exception is raised
+def f():
+    raise SystemExit(1)
+
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
